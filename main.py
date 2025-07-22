141f32c13b75c540afdf89611bdec7fafbd390a6
sidoulasbeur <ahmedlasbeur.09@gmail.com>
2025-07-22 10:32:17 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "أهلا بك في بوتي ✨")
+
+bot.polling()
